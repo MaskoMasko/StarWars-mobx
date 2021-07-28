a6876986482e1d5ba0388f3d1518dfedad28dc3a
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-28 13:30:55 +0200

    Update CharacterListScreen.js

--- screens/CharacterListScreen.js
+++ screens/CharacterListScreen.js
@@ -1,24 +1,44 @@
 import { observer } from "mobx-react";
 import * as React from "react";
 import { View, Text, ScrollView, TouchableOpacity } from "react-native";
-import { tamoNekiStore } from "../store/tamoNekiStore";
+import { store } from "../store/tamoNekiStore";
 
 export const CharacterListScreen = observer(({ navigation }) => {
   const url = "https://swapi.dev/api/people/?format=json";
 
   React.useEffect(() => {
-    tamoNekiStore.fetchingData(url);
+    store.fetchingData(url);
   }, []);
 
   return (
     <ScrollView>
-      {tamoNekiStore.state.dataFetched.map((char, id) => {
-        const { name } = char;
+      {store.state.dataFetched.map((char, id) => {
+        const {
+          name,
+          birth_year,
+          eye_color,
+          hair_color,
+          mass,
+          height,
+          skin_color,
+          homeworld,
+          gender,
+        } = char;
         return (
           <TouchableOpacity
             onPress={() => {
               navigation.navigate("CharDetail");
-              tamoNekiStore.selectedChar(name);
+              store.selectedChar(
+                name,
+                birth_year,
+                eye_color,
+                hair_color,
+                mass,
+                height,
+                skin_color,
+                homeworld,
+                gender
+              );
             }}
             key={id}
             style={{ width: 200, height: 200, backgroundColor: "red" }}
