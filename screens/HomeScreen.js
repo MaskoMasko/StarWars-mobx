ef154151c014aef361d795d199f62f335135f728
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-28 13:30:51 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,28 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import * as React from "react";
+import { View, Text, Button, Modal, TextInput } from "react-native";
 
 export function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Hello USER</Text>
-      <Button title="Go To CharcterList Screen" onPress={() => navigation.navigate("CharList")}
-      ></Button>
+  const [userName, setUserName] = React.useState("");
+  const [loginModal, setLoginModal] = React.useState(true);
 
-      <Button title="Go To FavCharcterList Screen" onPress={() => navigation.navigate("FavList")}
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Modal visible={loginModal}>
+        <TextInput
+          placeholder="Enter Usename"
+          value={userName}
+          onChangeText={(e) => setUserName(e)}
+        ></TextInput>
+        <Button title="submit" onPress={() => setLoginModal(false)}></Button>
+      </Modal>
+      <Text>Hello {userName}!</Text>
+      <Button
+        title="Go To CharcterList Screen"
+        onPress={() => navigation.navigate("CharList")}
+      ></Button>
+      <Button
+        title="Go To FavCharcterList Screen"
+        onPress={() => navigation.navigate("FavList")}
       ></Button>
     </View>
   );
