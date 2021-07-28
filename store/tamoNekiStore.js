036eac21bcaba0016f0533d0ac297d136ba9a321
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-28 13:30:48 +0200

    Update tamoNekiStore.js

--- store/tamoNekiStore.js
+++ store/tamoNekiStore.js
@@ -2,7 +2,18 @@ import { makeAutoObservable, observable, action, flow } from "mobx";
 
 const state = observable({
   dataFetched: [],
-  charDetails: undefined,
+  charDetails: {
+    name: undefined,
+    birth_year: undefined,
+    eye_color: undefined,
+    // films: undefined,
+    gender: undefined,
+    hair_color: undefined,
+    height: undefined,
+    homeworld: undefined,
+    mass: undefined,
+    skin_color: undefined,
+  },
   favCharList: [],
 });
 
@@ -17,11 +28,29 @@ const addChar = action(function addChar(name) {
   state.favCharList.push(name);
 });
 
-const selectedChar = action(function selectedChar(name) {
-  state.charDetails = name;
+const selectedChar = action(function selectedChar(
+  name,
+  birth_year,
+  eye_color,
+  hair_color,
+  mass,
+  height,
+  skin_color,
+  homeworld,
+  gender
+) {
+  state.charDetails.name = name;
+  state.charDetails.birth_year = birth_year;
+  state.charDetails.eye_color = eye_color;
+  state.charDetails.hair_color = hair_color;
+  state.charDetails.mass = mass;
+  state.charDetails.height = height;
+  state.charDetails.skin_color = skin_color;
+  state.charDetails.homeworld = homeworld;
+  state.charDetails.gender = gender;
 });
 
-export const tamoNekiStore = {
+export const store = {
   state,
   fetchingData,
   addChar,
