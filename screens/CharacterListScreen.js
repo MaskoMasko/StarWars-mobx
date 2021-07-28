b1aedc1deb681ab80ed1862378297a235d652859
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-28 14:00:54 +0200

    Update CharacterListScreen.js

--- screens/CharacterListScreen.js
+++ screens/CharacterListScreen.js
@@ -1,6 +1,12 @@
 import { observer } from "mobx-react";
 import * as React from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { store } from "../store/tamoNekiStore";
 
 export const CharacterListScreen = observer(({ navigation }) => {
@@ -11,42 +17,73 @@ export const CharacterListScreen = observer(({ navigation }) => {
   }, []);
 
   return (
-    <ScrollView>
-      {store.state.dataFetched.map((char, id) => {
-        const {
-          name,
-          birth_year,
-          eye_color,
-          hair_color,
-          mass,
-          height,
-          skin_color,
-          homeworld,
-          gender,
-        } = char;
-        return (
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("CharDetail");
-              store.selectedChar(
-                name,
-                birth_year,
-                eye_color,
-                hair_color,
-                mass,
-                height,
-                skin_color,
-                homeworld,
-                gender
-              );
-            }}
-            key={id}
-            style={{ width: 200, height: 200, backgroundColor: "red" }}
-          >
-            <Text>{name}</Text>
-          </TouchableOpacity>
-        );
-      })}
-    </ScrollView>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgb(46, 49, 49)",
+      }}
+    >
+      <ScrollView style={styles.charItemContainer}>
+        {store.state.dataFetched.map((char, id) => {
+          const {
+            name,
+            birth_year,
+            eye_color,
+            hair_color,
+            mass,
+            height,
+            skin_color,
+            homeworld,
+            gender,
+          } = char;
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("CharDetail");
+                store.selectedChar(
+                  name,
+                  birth_year,
+                  eye_color,
+                  hair_color,
+                  mass,
+                  height,
+                  skin_color,
+                  homeworld,
+                  gender
+                );
+              }}
+              key={id}
+              style={styles.charListItem}
+              activeOpacity={0.5}
+            >
+              <Text style={styles.charListItemText}>{name}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    </View>
   );
+});
+
+const styles = StyleSheet.create({
+  charItemContainer: {
+    width: "100%",
+    backgroundColor: "rgb(46, 49, 49)",
+  },
+  charListItem: {
+    marginHorizontal: "3%",
+    width: "94%",
+    height: 50,
+    backgroundColor: "yellow",
+    margin: 15,
+    borderRadius: 10,
+    justifyContent: "center",
+  },
+  charListItemText: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
