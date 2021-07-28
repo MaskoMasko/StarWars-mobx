dfe31dccb5f0fa84a52fd1db1e61b352caa4ff0f
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-28 13:30:53 +0200

    Update FavCharacterListScreen.js

--- screens/FavCharacterListScreen.js
+++ screens/FavCharacterListScreen.js
@@ -1,13 +1,13 @@
 import { observer } from "mobx-react";
 import * as React from "react";
 import { View, Text, ScrollView, Button } from "react-native";
-import { tamoNekiStore } from "../store/tamoNekiStore";
+import { store } from "../store/tamoNekiStore";
 
 export const FavCharacterListScreen = observer(({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <ScrollView>
-        {tamoNekiStore.state.favCharList.map((char, id) => {
+        {store.state.favCharList.map((char, id) => {
           return <Text key={id}>{char}</Text>;
         })}
       </ScrollView>
