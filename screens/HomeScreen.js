8664d8f34bd12f5412fec0ae03ab71a372c24f30
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-28 13:40:45 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,14 @@
 import * as React from "react";
-import { View, Text, Button, Modal, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
 
 export function HomeScreen({ navigation }) {
   const [userName, setUserName] = React.useState("");
@@ -8,12 +17,31 @@ export function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Modal visible={loginModal}>
-        <TextInput
-          placeholder="Enter Usename"
-          value={userName}
-          onChangeText={(e) => setUserName(e)}
-        ></TextInput>
-        <Button title="submit" onPress={() => setLoginModal(false)}></Button>
+        <View style={styles.nameModal}>
+          <Image
+            style={{ width: 300, marginBottom: 100, height: 200 }}
+            source={{
+              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/640px-Star_Wars_Logo.svg.png",
+            }}
+          ></Image>
+          <TextInput
+            style={styles.inputField}
+            value={userName}
+            placeholder="Enter Your Name"
+            onChangeText={(e) => setUserName(e.replace(/ /g, ""))}
+          ></TextInput>
+          <TouchableOpacity
+            style={styles.nameSubmitButton}
+            activeOpacity={0.5}
+            onPress={() => {
+              userName !== ""
+                ? setLoginModal(false)
+                : Alert.alert("Enter Username");
+            }}
+          >
+            <Text style={styles.nameSubmitButtonText}>SUBMIT NAME</Text>
+          </TouchableOpacity>
+        </View>
       </Modal>
       <Text>Hello {userName}!</Text>
       <Button
@@ -27,3 +55,29 @@ export function HomeScreen({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  nameModal: {
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+  inputField: {
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+    backgroundColor: "rgba(240, 240, 214, 1)",
+  },
+  nameSubmitButton: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "yellow",
+    marginTop: 20,
+  },
+  nameSubmitButtonText: {
+    color: "black",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
