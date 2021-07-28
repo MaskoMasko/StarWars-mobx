ef15c75eb4c7aba24c221b8df09116b94bb05624
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-28 13:30:57 +0200

    Update CharacterDetailScreen.js

--- screens/CharacterDetailScreen.js
+++ screens/CharacterDetailScreen.js
@@ -1,18 +1,36 @@
 import { observer } from "mobx-react";
 import * as React from "react";
 import { View, Text, Button } from "react-native";
-import { tamoNekiStore } from "../store/tamoNekiStore";
+import { store } from "../store/tamoNekiStore";
 
 export const CharacterDetailScreen = observer(({ navigation }) => {
-  const name = tamoNekiStore.state.charDetails;
+  const {
+    name,
+    birth_year,
+    eye_color,
+    hair_color,
+    mass,
+    height,
+    skin_color,
+    homeworld,
+    gender,
+  } = store.state.charDetails;
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>{name}</Text>
+      <Text>{birth_year}</Text>
+      <Text>{eye_color}</Text>
+      <Text>{hair_color}</Text>
+      <Text>{mass}</Text>
+      <Text>{height}</Text>
+      <Text>{skin_color}</Text>
+      <Text>{homeworld}</Text>
+      <Text>{gender}</Text>
       <Button
         title="add ot fav"
         onPress={() => {
           navigation.navigate("FavList");
-          tamoNekiStore.addChar(name);
+          store.addChar(name);
         }}
       ></Button>
     </View>
