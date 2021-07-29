beed551a223ae44bb60ec5d86369382af9b8c70e
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-29 08:14:39 +0200

    Update CharacterDetailScreen.js

--- screens/CharacterDetailScreen.js
+++ screens/CharacterDetailScreen.js
@@ -1,9 +1,19 @@
 import { observer } from "mobx-react";
+import { useRoute } from "@react-navigation/native";
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { store } from "../store/tamoNekiStore";
 
 export const CharacterDetailScreen = observer(({ navigation }) => {
+  const route = useRoute();
+
   React.useEffect(() => {
     store.fetchingCharacterMovies(store.state.charId);
   }, []);
@@ -19,29 +29,103 @@ export const CharacterDetailScreen = observer(({ navigation }) => {
     gender,
   } = store.state.charDetails;
   return (
-    <View style={{ flex: 1 }}>
-      <Text>{name}</Text>
-      <Text>Personal</Text>
-      <Text>Gender: {gender}</Text>
-      <Text>Brith Year: {birth_year}</Text>
-      <Text>Height: {height} cm</Text>
-      <Text>Mass: {mass} kg</Text>
-      <Text>Eye Color: {eye_color}</Text>
-      <Text>Hair Color: {hair_color}</Text>
-      <Text>Skin Color: {skin_color}</Text>
-      <Text>Movies</Text>
+    <ScrollView
+      style={{
+        flex: 1,
+        paddingLeft: 10,
+        backgroundColor: "rgb(46, 49, 49)",
+      }}
+    >
+      <Text style={styles.characterName}>{name}</Text>
+      <Text style={styles.characterPodnaslovi}>Personal</Text>
+      <Text style={styles.characterDrugo}>Gender: {gender}</Text>
+      <Text style={styles.characterDrugo}>Brith Year: {birth_year}</Text>
+      <Text style={styles.characterDrugo}>Height: {height} cm</Text>
+      <Text style={styles.characterDrugo}>Mass: {mass} kg</Text>
+      <Text style={styles.characterDrugo}>Eye Color: {eye_color}</Text>
+      <Text style={styles.characterDrugo}>Hair Color: {hair_color}</Text>
+      <Text style={styles.characterDrugo}>Skin Color: {skin_color}</Text>
+      <Text style={styles.characterPodnaslovi}>Movies</Text>
       <Text>
         {store.state.movies.map((movie, id) => {
-          return <Text key={id}>{movie}</Text>;
+          return (
+            <View key={id}>
+              {/* HOW TO AVOID PRETTIER? */}
+              <Text style={styles.characterDrugo}>{movie} </Text>
+            </View>
+          );
         })}
       </Text>
-      <Button
-        title="add ot fav"
-        onPress={() => {
-          navigation.navigate("FavList");
-          store.addChar(name);
-        }}
-      ></Button>
-    </View>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => {
+            navigation.navigate("CharList");
+          }}
+          style={[
+            styles.addToFavBtn,
+            {
+              marginHorizontal: 10,
+              marginVertical: 20,
+            },
+          ]}
+        >
+          <Text style={styles.addToFavBtnText}>GO BACK</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => {
+            navigation.navigate("FavList");
+            store.addChar(name);
+          }}
+          style={[
+            styles.addToFavBtn,
+            {
+              backgroundColor: "yellow",
+              marginHorizontal: 10,
+              marginVertical: 20,
+            },
+          ]}
+        >
+          <Text
+            style={[
+              styles.addToFavBtnText,
+              {
+                color: "black",
+              },
+            ]}
+          >
+            ADD TO FAVORITES
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
+});
+
+const styles = StyleSheet.create({
+  characterName: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+  },
+  characterPodnaslovi: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  characterDrugo: {
+    fontSize: 20,
+    color: "white",
+  },
+  addToFavBtn: {
+    backgroundColor: "black",
+    padding: 20,
+    borderRadius: 10,
+  },
+  addToFavBtnText: {
+    color: "white",
+    alignSelf: "center",
+    fontWeight: "bold",
+  },
 });
