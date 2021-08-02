7d93680d80ea6e80b31f7bae55906a7f7352f441
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-08-02 14:48:21 +0200

    Update FavCharacterListScreen.js

--- screens/FavCharacterListScreen.js
+++ screens/FavCharacterListScreen.js
@@ -14,13 +14,81 @@ import { characterStore, store } from "../store/tamoNekiStore";
 
 export const FavCharacterListScreen = observer(({ navigation }) => {
   return (
-    <View>
-      {characterStore.favoriteCharacterList.map((e, i) => {
-        return <Text key={i}>{e.name}</Text>;
-      })}
+    <View
+      style={[
+        styles.charItemContainer,
+        { flex: 1, alignItems: "center", justifyContent: "center" },
+      ]}
+    >
+      <ScrollView>
+        {characterStore.favoriteCharacterList.length == 0 ? (
+          <Text style={styles.emptyListText}>
+            You Haven't Added Any Character To The List...
+          </Text>
+        ) : (
+          characterStore.favoriteCharacterList.map((char, id) => {
+            return (
+              <View
+                key={id}
+                style={[
+                  styles.charListItem,
+                  {
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: "78%",
+                    alignItems: "center",
+                  },
+                ]}
+              >
+                <Text style={styles.charListItemText}>{char.name}</Text>
+                <TouchableOpacity
+                  activeOpacity={0.5}
+                  onPress={() => {
+                    characterStore.removeCharacterFromList(id);
+                  }}
+                  style={{
+                    padding: 10,
+                    backgroundColor: "black",
+                    borderRadius: 5,
+                    margin: 5,
+                  }}
+                >
+                  <Text style={{ color: "white", fontWeight: "bold" }}>
+                    REMOVE
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            );
+          })
+        )}
+      </ScrollView>
+      {/* <Button title="Save List" onPress={() => store.saveValue()}></Button> */}
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={() => navigation.popToTop()}
+        style={{
+          backgroundColor: "black",
+          padding: 20,
+          borderRadius: 10,
+          width: "60%",
+          margin: 10,
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            alignSelf: "center",
+            fontSize: 22,
+            fontWeight: "bold",
+          }}
+        >
+          HOME
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 });
+
 //   return (
 //     <View
 //       style={[
