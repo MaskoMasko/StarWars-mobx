b12cca1b3b0810da7717c6b38356126e9ed0ddde
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-08-02 10:14:05 +0200

    Update CharacterListScreen.js

--- screens/CharacterListScreen.js
+++ screens/CharacterListScreen.js
@@ -130,24 +130,13 @@ export const CharacterListScreen = observer(({ navigation }) => {
 
   return (
     <ScrollView style={styles.charItemContainer}>
-      {characterStore.dataFetched.map((char, id) => {
-        const { name } = char;
+      {characterStore.characterList.map((char, id) => {
+        const { name, url } = char;
         return (
           <TouchableOpacity
             onPress={() => {
+              characterStore.setSelecterCharacter(url);
               navigation.navigate("CharDetail");
-              characterStore.selectedChar({
-                name,
-                //   birth_year,
-                //   eye_color,
-                //   hair_color,
-                //   mass,
-                //   height,
-                //   skin_color,
-                //   gender,
-              });
-              // store.state.charId = id;
-              // store.state.movies = [];
             }}
             key={id}
             style={styles.charListItem}
